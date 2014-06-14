3daa3820c0af5888d061394a7f5db1c139b825f3
trikitrok <trikitrok@gmail.com>
2014-06-14 12:57:48 +0200

    Refactor: removed some duplication

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -4,21 +4,27 @@
 
 (defn show [time]
   (let
-    [
-     [h m s] (map #(Integer. %) (split time #":"))
+    [[h m s] (map #(Integer. %) (split time #":"))
 
      show-seconds (fn [s]
                     (if (zero? (rem s 2))
                       "Y" "R"))
 
-     turn-on (fn [lamps] (repeat lamps "R"))
+     turn-on (fn [num-lamps color] (repeat num-lamps color))
 
-     turn-off (fn [lamps] (repeat lamps "O"))
+     turn-on-red (fn [num-lamps] (turn-on num-lamps "R"))
 
-     show-hours (fn [lamps-on]
-                  (let [lamps-off (- 4 lamps-on)]
-                    (apply str (concat (turn-on lamps-on)
-                                       (turn-off lamps-off)))))
+     turn-on-yellow (fn [num-lamps] (turn-on num-lamps "Y"))
+
+     turn-off (fn [num-lamps] (repeat num-lamps "O"))
+
+     show (fn [lamps-on lamps-off]
+            (apply str (concat lamps-on lamps-off)))
+
+     show-hours (fn [num-lamps-on]
+                  (let [num-lamps-off (- 4 num-lamps-on)]
+                    (show (turn-on-red num-lamps-on)
+                          (turn-off num-lamps-off))))
 
      show-hours-first-row (fn [h]
                             (show-hours (quot h 5)))
@@ -27,16 +33,16 @@
                              (show-hours (rem h 5)))
 
      show-minutes-first-row (fn [m]
-                              (let [lamps-on (quot m 5)
-                                    lamps-off (- 11 lamps-on)]
-                                (apply str (concat (take lamps-on (cycle ["Y" "Y" "R"]))
-                                                   (turn-off lamps-off)))))
+                              (let [num-lamps-on (quot m 5)
+                                    num-lamps-off (- 11 num-lamps-on)]
+                                (show (take num-lamps-on (cycle ["Y" "Y" "R"]))
+                                      (turn-off num-lamps-off))))
 
      show-minutes-second-row (fn [m]
                                (let [lamps-on (rem m 5)
-                                    lamps-off (- 4 lamps-on)]
-                                 (apply str (concat (repeat lamps-on "Y")
-                                                   (turn-off lamps-off)))))]
+                                     lamps-off (- 4 lamps-on)]
+                                 (show (turn-on-yellow lamps-on)
+                                       (turn-off lamps-off))))]
 
     (join "\n"
           [(show-seconds s)
