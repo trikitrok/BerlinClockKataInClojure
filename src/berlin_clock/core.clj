636f6e68c0c82100786b33823b807b4e1a9dd305
trikitrok <trikitrok@gmail.com>
2014-06-14 11:42:03 +0200

    Refactor: renamed helpers

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -5,16 +5,17 @@
 (defn show [time]
   (let
     [[h m s] (map #(Integer. %) (split time #":"))
-     seconds-lamp (fn [s]
+
+     show-seconds (fn [s]
                     (if (zero? (rem s 2))
                       "Y" "R"))
 
-     hours-first-row (fn [h]
+     show-hours-first-row (fn [h]
                        (apply str (concat (repeat (quot h 5) "R")(repeat (- 4 (quot h 5)) "O"))))]
 
     (join "\n"
-          [(seconds-lamp s)
-           (hours-first-row h)
+          [(show-seconds s)
+           (show-hours-first-row h)
            "OOOO"
            "OOOOOOOOOOO"
            "OOOO"])))
