92740a76ce59d7a9eae5bd325a39c18b831721b7
trikitrok <trikitrok@gmail.com>
2014-06-14 11:17:16 +0200

    Seconds lamp is working

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -1,11 +1,16 @@
 (ns berlin_clock.core)
 
-(use '[clojure.string :only (join)])
+(use '[clojure.string :only (join split)])
 
 (defn show [time]
-  (join "\n"
-        ["Y"
-         "OOOO"
-         "OOOO"
-         "OOOOOOOOOOO"
-         "OOOO"]))
+  (let
+    [[h m s] (map #(Integer. %) (split time #":"))
+     seconds-lamp (fn [s]
+                    (if (= 0 (rem s 2))
+                      "Y" "R"))]
+    (join "\n"
+          [(seconds-lamp s)
+           "OOOO"
+           "OOOO"
+           "OOOOOOOOOOO"
+           "OOOO"])))
--- test/berlin_clock/t_core.clj
+++ test/berlin_clock/t_core.clj
@@ -5,11 +5,19 @@
 (use '[clojure.string :only (join)])
 
 (facts "about Berlin Clock"
+       (facts "seconds lamp"
+              (fact "It turns off for one second (Y)"
+                    (show "00:00:00") => (join "\n"
+                                               ["Y"
+                                                "OOOO"
+                                                "OOOO"
+                                                "OOOOOOOOOOO"
+                                                "OOOO"]))
 
-  (fact "it tells the time in a funny way"
-    (show "00:00:00") => (join "\n"
-                               ["Y"
-                               "OOOO"
-                               "OOOO"
-                               "OOOOOOOOOOO"
-                               "OOOO"])))
+              (fact "It turns on for the next second (R)"
+                    (show "00:00:01") => (join "\n"
+                                               ["R"
+                                                "OOOO"
+                                                "OOOO"
+                                                "OOOOOOOOOOO"
+                                                "OOOO"]))))
